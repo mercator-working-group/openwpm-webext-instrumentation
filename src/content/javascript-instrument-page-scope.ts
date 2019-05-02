cb1b0c74ef049be28cdef1f19039764a24af25c4
Fredrik Wollsén <fredrik@neam.se>
2019-05-02 17:51:45 +0300

    Added first version of Prebid.js instrumentation

--- src/content/javascript-instrument-page-scope.ts
+++ src/content/javascript-instrument-page-scope.ts
@@ -29,6 +29,40 @@ export const pageScript = function({
     (window as any).instrumentObject = instrumentObject;
   }
 
+  /**
+   * Prebid.js
+   */
+
+  // Wait for window.pbjs to be available
+  const prebidJsAvailable = new Promise(function (resolve) {
+    const now = Date.now();
+      function check() {
+        if (typeof (window as any).pbjs !== "undefined") {
+          return resolve();
+        } else {
+          // Keep checking for 10 seconds
+          if (Date.now() - now < 10000) {
+            setTimeout(check, 10);
+          } else {
+            console.log("Stopped checking for Prebid.js loading on this page (10s have passed)");
+          }
+        }
+      }
+      check();
+    });
+
+  prebidJsAvailable.then(function () {
+    console.log("Prebid.js available - instrumenting...");
+    instrumentObject(
+      (window as any).pbjs,
+      "pbjs",
+      {},
+    );
+  });
+
+  // TODO instrument prebid.js events
+  //
+
   /*
    * Start Instrumentation
    */
